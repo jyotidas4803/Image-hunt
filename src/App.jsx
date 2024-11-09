656367d660ec4d56f37529284283b6752a97e1ab
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-11-09 14:44:52 +0000

    tailwind and vite setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
